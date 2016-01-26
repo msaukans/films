072b7da6bd2be282938d16ef20cfaa4a876b01ac
unknown <c12335876@BUS-1025-33.ict.ad.dit.ie>
2016-01-26 15:07:55 +0000

    validation added

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,6 @@
 class Genre < ActiveRecord::Base
 	has_many :movies
+	
+	validates :name, uniqueness: true
+	
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ActiveRecord::Base
 	belongs_to :genre, dependent: :destroy
-
+	
+	validates :title, :released_on, presence: true
 end
