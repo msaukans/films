a32187186bc6f6e30b594d54072deecddf2a12a2
msaukans <msaukans@gmail.com>
2016-02-02 15:27:47 +0000

    now login/logou

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+	user = User.find_by_email(params[:email])
+	if user && user.authenticate(params[:password])
+		session[:user_id]=user.id
+		redirect_to user
+	else
+		flash.now[:error] = "Invalid name/password combination."
+		render 'new'
+		end
+  end
+
+  def destroy
+	if logged_in?
+		session[:user_id] = nil
+	else
+		flash[:notice] = "You need to log in first"
+		render 'new'
+	end 
+	redirect_to login_path
+  end
+  
+  def new
+  end
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,13 @@ class Movie < ActiveRecord::Base
 	belongs_to :genre, dependent: :destroy
 	
 	validates :title, :released_on, presence: true
+	
+	validate :must_have_trailer
+	def must_have_trailer
+		unless trailer.include?("<iframe")
+			errors.add(:trailer, "must include an iframe tag")
+		end
+	end
+	
+	
 end
